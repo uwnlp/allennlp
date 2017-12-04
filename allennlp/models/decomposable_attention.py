7520fbedf4529bb51d19b320b1997262ac98b6c0
kernelmachine <sgrangan01@gmail.com>
2017-12-04 13:40:34 -0800

    update

--- allennlp/models/decomposable_attention.py
+++ allennlp/models/decomposable_attention.py
@@ -6,12 +6,12 @@ from allennlp.common import Params
 from allennlp.common.checks import ConfigurationError
 from allennlp.data import Vocabulary
 from allennlp.models.model import Model
-from allennlp.modules import FeedForward, MatrixAttention
+from allennlp.modules import FeedForward, MatrixAttention, Highway
 from allennlp.modules import Seq2SeqEncoder, SimilarityFunction, TimeDistributed, TextFieldEmbedder
 from allennlp.nn import InitializerApplicator, RegularizerApplicator
 from allennlp.nn.util import get_text_field_mask, last_dim_softmax, weighted_sum
 from allennlp.training.metrics import CategoricalAccuracy
-
+from allennlp.modules.elmo import _ElmoCharacterEncoder
 
 @Model.register("decomposable_attention")
 class DecomposableAttention(Model):
@@ -24,10 +24,12 @@ class DecomposableAttention(Model):
     before doing the decomposable entailment step.  We generalize this to any
     :class:`Seq2SeqEncoder` that can be applied to the premise and/or the hypothesis before
     computing entailment.
+
     The basic outline of this model is to get an embedded representation of each word in the
     premise and hypothesis, align words between the two, compare the aligned phrases, and make a
     final entailment decision based on this aggregated comparison.  Each step in this process uses
     a feedforward network to modify the representation.
+
     Parameters
     ----------
     vocab : ``Vocabulary``
@@ -68,22 +70,24 @@ class DecomposableAttention(Model):
                  hypothesis_encoder: Optional[Seq2SeqEncoder] = None,
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None) -> None:
-        super(DecomposableAttention, self).__init__(vocab, regularizer)
+        super(DecomposableAttentionUW, self).__init__(vocab, regularizer)
 
         self._text_field_embedder = text_field_embedder
+        self._highway_layer = TimeDistributed(Highway(text_field_embedder.get_output_dim(),
+                                                      3))
         self._attend_feedforward = TimeDistributed(attend_feedforward)
         self._matrix_attention = MatrixAttention(similarity_function)
         self._compare_feedforward = TimeDistributed(compare_feedforward)
         self._aggregate_feedforward = aggregate_feedforward
-        self._premise_encoder = premise_encoder
-        self._hypothesis_encoder = hypothesis_encoder or premise_encoder
+        # self._premise_encoder = premise_encoder
+        self._hypothesis_encoder = hypothesis_encoder #or premise_encoder
 
         self._num_labels = vocab.get_vocab_size(namespace="labels")
 
-        if text_field_embedder.get_output_dim() != attend_feedforward.get_input_dim():
-            raise ConfigurationError("Output dimension of the text_field_embedder (dim: {}), "
+        if hypothesis_encoder.get_output_dim() != attend_feedforward.get_input_dim():
+            raise ConfigurationError("Output dimension of the hypothesis_encoder (dim: {}), "
                                      "must match the input_dim of the FeedForward layer "
-                                     "attend_feedforward, (dim: {}). ".format(text_field_embedder.get_output_dim(),
+                                     "attend_feedforward, (dim: {}). ".format(hypothesis_encoder.get_output_dim(),
                                                                               attend_feedforward.get_input_dim()))
         if aggregate_feedforward.get_output_dim() != self._num_labels:
             raise ConfigurationError("Final output dimension (%d) must equal num labels (%d)" %
@@ -91,7 +95,7 @@ class DecomposableAttention(Model):
 
         self._accuracy = CategoricalAccuracy()
         self._loss = torch.nn.CrossEntropyLoss()
-
+        
         initializer(self)
 
     def forward(self,  # type: ignore
@@ -108,9 +112,11 @@ class DecomposableAttention(Model):
             From a ``TextField``
         label : torch.IntTensor, optional (default = None)
             From a ``LabelField``
+
         Returns
         -------
         An output dictionary consisting of:
+
         label_logits : torch.FloatTensor
             A tensor of shape ``(batch_size, num_labels)`` representing unnormalised log
             probabilities of the entailment label.
@@ -120,20 +126,23 @@ class DecomposableAttention(Model):
         loss : torch.FloatTensor, optional
             A scalar loss to be optimised.
         """
-        embedded_premise = self._text_field_embedder(premise)
-        embedded_hypothesis = self._text_field_embedder(hypothesis)
-        premise_mask = get_text_field_mask(premise).float()
+        
+        # embedded_premise = self._text_field_embedder(premise)
+        embedded_hypothesis = self._highway_layer(self._text_field_embedder(hypothesis))    
+        # premise_mask = get_text_field_mask(premise).float()
         hypothesis_mask = get_text_field_mask(hypothesis).float()
+        # if self._premise_encoder:
+        #     embedded_premise = self._premise_encoder(embedded_premise, premise_mask)
+        embedded_hypothesis = self._hypothesis_encoder(embedded_hypothesis, hypothesis_mask)
 
-        if self._premise_encoder:
-            embedded_premise = self._premise_encoder(embedded_premise, premise_mask)
-        if self._hypothesis_encoder:
-            embedded_hypothesis = self._hypothesis_encoder(embedded_hypothesis, hypothesis_mask)
 
-        projected_premise = self._attend_feedforward(embedded_premise)
+
+        # projected_premise = self._attend_feedforward(embedded_premise)
         projected_hypothesis = self._attend_feedforward(embedded_hypothesis)
+
         # Shape: (batch_size, premise_length, hypothesis_length)
-        similarity_matrix = self._matrix_attention(projected_premise, projected_hypothesis)
+        # similarity_matrix = self._matrix_attention(projected_premise, projected_hypothesis)
+        similarity_matrix = self._matrix_attention(projected_hypothesis, projected_hypothesis)
 
         # Shape: (batch_size, premise_length, hypothesis_length)
         p2h_attention = last_dim_softmax(similarity_matrix, hypothesis_mask)
@@ -141,24 +150,25 @@ class DecomposableAttention(Model):
         attended_hypothesis = weighted_sum(embedded_hypothesis, p2h_attention)
 
         # Shape: (batch_size, hypothesis_length, premise_length)
-        h2p_attention = last_dim_softmax(similarity_matrix.transpose(1, 2).contiguous(), premise_mask)
-        # Shape: (batch_size, hypothesis_length, embedding_dim)
-        attended_premise = weighted_sum(embedded_premise, h2p_attention)
+        # h2p_attention = last_dim_softmax(similarity_matrix.transpose(1, 2).contiguous(), premise_mask)
+        # # Shape: (batch_size, hypothesis_length, embedding_dim)
+        # attended_premise = weighted_sum(embedded_premise, h2p_attention)
+        #
+        # premise_compare_input = torch.cat([embedded_premise, attended_hypothesis], dim=-1)
+        # hypothesis_compare_input = torch.cat([embedded_hypothesis, attended_premise], dim=-1)
+        hypothesis_compare_input = torch.cat([embedded_hypothesis, attended_hypothesis], dim=-1)
 
-        premise_compare_input = torch.cat([embedded_premise, attended_hypothesis], dim=-1)
-        hypothesis_compare_input = torch.cat([embedded_hypothesis, attended_premise], dim=-1)
-
-        compared_premise = self._compare_feedforward(premise_compare_input)
-        compared_premise = compared_premise * premise_mask.unsqueeze(-1)
-        # Shape: (batch_size, compare_dim)
-        compared_premise = compared_premise.sum(dim=1)
+        # compared_premise = self._compare_feedforward(premise_compare_input)
+        # compared_premise = compared_premise * premise_mask.unsqueeze(-1)
+        # # Shape: (batch_size, compare_dim)
+        # compared_premise = compared_premise.sum(dim=1)
 
         compared_hypothesis = self._compare_feedforward(hypothesis_compare_input)
         compared_hypothesis = compared_hypothesis * hypothesis_mask.unsqueeze(-1)
         # Shape: (batch_size, compare_dim)
         compared_hypothesis = compared_hypothesis.sum(dim=1)
 
-        aggregate_input = torch.cat([compared_premise, compared_hypothesis], dim=-1)
+        aggregate_input = torch.cat([compared_hypothesis, compared_hypothesis], dim=-1)
         label_logits = self._aggregate_feedforward(aggregate_input)
         label_probs = torch.nn.functional.softmax(label_logits)
 
@@ -177,15 +187,16 @@ class DecomposableAttention(Model):
                 }
 
     @classmethod
-    def from_params(cls, vocab: Vocabulary, params: Params) -> 'DecomposableAttention':
+    def from_params(cls, vocab: Vocabulary, params: Params) -> 'DecomposableAttentionUW':
         embedder_params = params.pop("text_field_embedder")
         text_field_embedder = TextFieldEmbedder.from_params(vocab, embedder_params)
+       
 
-        premise_encoder_params = params.pop("premise_encoder", None)
-        if premise_encoder_params is not None:
-            premise_encoder = Seq2SeqEncoder.from_params(premise_encoder_params)
-        else:
-            premise_encoder = None
+        # premise_encoder_params = params.pop("premise_encoder", None)
+        # if premise_encoder_params is not None:
+        #     premise_encoder = Seq2SeqEncoder.from_params(premise_encoder_params)
+        # else:
+        #     premise_encoder = None
 
         hypothesis_encoder_params = params.pop("hypothesis_encoder", None)
         if hypothesis_encoder_params is not None:
@@ -206,7 +217,7 @@ class DecomposableAttention(Model):
                    similarity_function=similarity_function,
                    compare_feedforward=compare_feedforward,
                    aggregate_feedforward=aggregate_feedforward,
-                   premise_encoder=premise_encoder,
+                   # premise_encoder=premise_encoder,
                    hypothesis_encoder=hypothesis_encoder,
                    initializer=initializer,
                    regularizer=regularizer)
