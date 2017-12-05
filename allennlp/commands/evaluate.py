35fbff14f7ea7b03f31dabd6a7320d18b7b1ed9f
kernelmachine <sgrangan01@gmail.com>
2017-12-05 12:09:16 -0800

    update

--- allennlp/commands/evaluate.py
+++ allennlp/commands/evaluate.py
@@ -93,19 +93,19 @@ def evaluate(model: Model,
         for item in raw_fields:
             premise = " ".join([x.text for x in item['premise'].tokens]).replace("@@NULL@@", '')
             hypothesis = " ".join([x.text for x in item['hypothesis'].tokens]).replace("@@NULL@@", '')
-            # hypothesis_binary_parse = item["metadata_hypothesis_binary_parse"]
-            # premise_binary_parse = item["metadata_premise_binary_parse"]
+            hypothesis_binary_parse = item["metadata_hypothesis_binary_parse"]
+            premise_binary_parse = item["metadata_premise_binary_parse"]
             label = item['label'].label
             parsed_fields.append({"sentence1": premise,
                                   "sentence2": hypothesis,
-                                  "gold_label": label})
-                                #   "hypothesis_binary_parse": hypothesis_binary_parse,
-                                #   "premise_binary_parse": premise_binary_parse
+                                  "gold_label": label,
+                                  "sentence2_binary_parse": hypothesis_binary_parse,
+                                  "sentence1_binary_parse": premise_binary_parse})
 
         parsed_fields = pd.DataFrame(parsed_fields)
         tensor_batch = arrays_to_variables(batch, cuda_device, for_training=False)
-        # tensor_batch.pop('metadata_hypothesis_binary_parse', None)
-        # tensor_batch.pop('metadata_premise_binary_parse', None)
+        tensor_batch.pop('metadata_hypothesis_binary_parse', None)
+        tensor_batch.pop('metadata_premise_binary_parse', None)
         bo = model.forward(**tensor_batch)
         metrics = model.get_metrics()
         description = ', '.join(["%s: %.2f" % (name, value) for name, value in metrics.items()]) + " ||"
