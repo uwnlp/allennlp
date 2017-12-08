bbf9a320ae88f2cc675792bcdf7c8a5e27cb970a
kernelmachine <sgrangan01@gmail.com>
2017-12-07 22:42:20 -0800

    added thresh

--- allennlp/commands/evaluate.py
+++ allennlp/commands/evaluate.py
@@ -137,8 +137,10 @@ def evaluate(model: Model,
         parsed_output = pd.concat([parsed_fields, batch_output], axis=1)
         output = pd.concat([output, parsed_output], axis=0)
     import ipdb; ipdb.set_trace()
-    hard_subset = output.loc[output.gold_label != output.prediction_label]
-    easy_subset = output.loc[output.gold_label == output.prediction_label]
+    hard_subset = output.loc[(output.gold_label != output.prediction_label)
+                             & (output.prediction_score <= 0.4)]
+    easy_subset = output.loc[(output.gold_label == output.prediction_label)
+                             & (output.prediction_score > 0.4)]
     return model.get_metrics(), hard_subset, easy_subset
 
 
