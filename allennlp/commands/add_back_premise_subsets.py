db207d996a61efc0b74599838d2534332ca4fe96
Suchin Gururangan <sgrangan01@gmail.com>
2017-12-09 17:36:01 -0800

    allennlp commands

--- allennlp/commands/add_back_premise_subsets.py
+++ allennlp/commands/add_back_premise_subsets.py
@@ -5,13 +5,20 @@ from tqdm import tqdm
 if __name__ == '__main__':
 	for dataset in tqdm(DATASETS):
 		if DATASETS[dataset].get('easy_tampered') is not None:
-			df = pd.read_json(DATASETS[dataset]['easy_tampered'], lines=True)
-			import ipdb; ipdb.set_trace()
-			df = df.rename(columns={'sentence1': 'null_token'})
-			df = df.rename(columns={'real_premise': 'sentence1'})
-			df.to_json(DATASETS[dataset]['easy_untampered'], lines=True, orient='records')
+			df = pd.read_json(DATASETS[args.dataset]['original'])
+			df = df[['sentence1', 'pairID']]
+			easy = pd.read_json(DATASETS[dataset]['easy_tampered'], lines=True)
+			master = easy.merge(df, on='pairID')
+			master = master.rename(columns={'sentence1_x': 'sentence1', 'sentence1_y':'real_premise'})
+			master = master.rename(columns={'sentence1': 'null_token'})
+			master = master.rename(columns={'real_premise': 'sentence1'})
+			master.to_json(DATASETS[dataset]['easy_untampered'], lines=True, orient='records')
 		if DATASETS[dataset].get('hard_tampered') is not None:
-			df = pd.read_json(DATASETS[dataset]['hard_tampered'], lines=True)
-			df = df.rename(columns={'sentence1': 'null_token'})
-			df = df.rename(columns={'real_premise': 'sentence1'})
-			df.to_json(DATASETS[dataset]['hard_untampered'], lines=True, orient='records')
+			df = pd.read_json(DATASETS[args.dataset]['original'])
+			df = df[['sentence1', 'pairID']]
+			easy = pd.read_json(DATASETS[dataset]['hard_tampered'], lines=True)
+			master = easy.merge(df, on='pairID')
+			master = master.rename(columns={'sentence1_x': 'sentence1', 'sentence1_y':'real_premise'})
+			master = master.rename(columns={'sentence1': 'null_token'})
+			master = master.rename(columns={'real_premise': 'sentence1'})
+			master.to_json(DATASETS[dataset]['hard_untampered'], lines=True, orient='records')
--- allennlp/commands/create_subsets.py
+++ allennlp/commands/create_subsets.py
@@ -9,5 +9,9 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 	easy_fp = DATASETS[args.dataset]['easy_tampered']
 	hard_fp = DATASETS[args.dataset]['hard_tampered']
+	df = pd.read_json(DATASETS[args.dataset]['original'])
+	df = df[['sentence1', 'pairID']]
+	master = dev_hard.merge(, on='pairID')
+	master = master.rename(columns={'sentence1_x': 'sentence1', 'sentence1_y':'real_premise'})
 	print(" ".join(['mv', "easy_subset.json", easy_fp]))
 	print(" ".join(['mv', "hard_subset.json", hard_fp]))
