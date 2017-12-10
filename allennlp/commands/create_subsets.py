5d17710fc86c69bb88cae058e7b58f86b33a8bf9
Suchin Gururangan <sgrangan01@gmail.com>
2017-12-09 16:38:26 -0800

    allennlp commands

--- allennlp/commands/create_subsets.py
+++ allennlp/commands/create_subsets.py
@@ -4,6 +4,7 @@ import os
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Test and Subset')
 	parser.add_argument('--dataset')
+	args = parser.parse_args()
 	easy_fp = os.path.join(DATA_DIR, DATASETS[args.dataset]['easy_tampered'])
 	hard_fp = os.path.join(DATA_DIR, DATASETS[args.dataset]['hard_tampered'])
 	subprocess.Popen(['mv', "easy_subset.json", easy_fp])
