af911799fd35ee9cee40efc518225a65f33e9d2d
Suchin Gururangan <sgrangan01@gmail.com>
2017-12-09 17:22:38 -0800

    allennlp commands

--- allennlp/commands/add_back_premise_subsets.py
+++ allennlp/commands/add_back_premise_subsets.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
 	for dataset in tqdm(DATASETS):
 		if DATASETS[dataset].get('easy_tampered') is not None:
 			df = pd.read_json(DATASETS[dataset]['easy_tampered'], lines=True)
+			import ipdb; ipdb.set_trace()
 			df = df.rename(columns={'sentence1': 'null_token'})
 			df = df.rename(columns={'real_premise': 'sentence1'})
 			df.to_json(DATASETS[dataset]['easy_untampered'], lines=True, orient='records')
