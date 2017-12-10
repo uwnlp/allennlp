dc5ef030d37437d3c19354af41b74fc4d6e81a82
Suchin Gururangan <sgrangan01@gmail.com>
2017-12-09 16:17:49 -0800

    allennlp commands

--- allennlp/commands/test.py
+++ allennlp/commands/test.py
@@ -3,6 +3,15 @@ from allennlp.commands.config import *
 import argparse
 import os
 
+def execute(cmd):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Test and Subset')
 	parser.add_argument('--model')
@@ -20,10 +29,14 @@ if __name__ == '__main__':
 			   evaluation_data_file,
 			   "--subset"]
 	p = Popen(command, stdout=PIPE, stderr=PIPE, shell=True)
-	while p.poll() is None:
-	    l = p.stdout.readline() # This blocks until it receives a newline.
-	    print(l)
-	print(p.stdout.read())
+
+	for path in execute(command):
+    	print(path, end="")
+
+	# while p.poll() is None:
+	#     l = p.stdout.readline() # This blocks until it receives a newline.
+	#     print(l)
+	# print(p.stdout.read())
 	import ipdb; ipdb.set_trace()
 	easy_fp = os.path.join(DATA_DIR, DATASETS[args.dataset]['easy_tampered'])
 	hard_fp = os.path.join(DATA_DIR, DATASETS[args.dataset]['hard_tampered'])
