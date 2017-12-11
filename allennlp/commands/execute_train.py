56f7053b26e774adf0f8a34ecda3158b9925a806
Suchin Gururangan <sgrangan01@gmail.com>
2017-12-10 16:33:29 -0800

    added

--- allennlp/commands/execute_train.py
+++ allennlp/commands/execute_train.py
@@ -19,27 +19,40 @@ if __name__ == '__main__':
     parser.add_argument('--half', choices=[0, 1, 2], type=int, default=0)
     parser.add_argument('--gpu', choices=[0, 1, 2], type=int)
     args = parser.parse_args()
-    config_file = "/home/sg01/allennlp/training_config/c1_train_configs/{}_{}_{}.json".format(args.corpus, args.split, args.half)
     if args.corpus == 'mnli' and args.split == 'full':
         model_name = "multinli_0.9_train"
+        base_config['train_data_path'] = "multinli_0.9_train"
+        base_config['validation_data_path'] = "multinli_0.9_dev_matched"
     elif args.corpus == 'mnli' and args.split == 'half':
         if args.half == 1:
             model_name = "mnli_train_half_1"
+            base_config['train_data_path'] = "mnli_train_half_1"
+            base_config['validation_data_path'] = "multinli_0.9_dev_matched"
         elif args.half == 2:
             model_name = "mnli_train_half_2"
+            base_config['train_data_path'] = "mnli_train_half_2"
+            base_config['validation_data_path'] = "multinli_0.9_dev_matched"
         else:
-            raise Exception("invalid argument: ", args.half)
+            raise Exception("invalid argument")
     elif args.corpus == 'snli' and args.split == 'full':
         model_name = "snli_1.0_train"
+        base_config['train_data_path'] = "snli_1.0_train"
+        base_config['validation_data_path'] = "snli_1.0_dev"  
     elif args.corpus == 'snli' and args.split == 'half':
         if args.half == 1:
             model_name = "snli_train_half_1"
+            base_config['train_data_path'] = "snli_train_half_1"
+            base_config['validation_data_path'] = "snli_1.0_dev"  
         elif args.half == 2:
             model_name = "snli_train_half_2"
+            base_config['train_data_path'] = "snli_train_half_2"
+            base_config['validation_data_path'] = "snli_1.0_dev"
         else:
-            raise Exception("invalid argument: ", args.half)
+            raise Exception("invalid argument")
     else:
-        raise Exception("invalid argument: ", args.corpus) 
+        raise Exception("invalid argument") 
+    config_file = "/home/sg01/allennlp/training_config/c1_train_configs/{}_{}_{}.json".format(args.corpus, args.split, args.half)
+    pd.to_json(config_file, lines=True, orient='records')  
     serialization_dir = "/home/sg01/allennlp/final_logs/{}".format(model_name) 
     command = ["CUDA_VISIBLE_DEVICES={}".format(args.gpu),
                "python -m allennlp.run train",
@@ -47,7 +60,6 @@ if __name__ == '__main__':
                "--serialization-dir",
                serialization_dir,
                "--c1"]
-
     print(" ".join(command))
     for path in execute(command):
         print(path, end="")
